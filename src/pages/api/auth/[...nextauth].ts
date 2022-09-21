7190bf436d27053f271410d6988b18be09fe8160
Paul Maier <pauljustus279@gmail.com>
2022-09-22 00:01:10 +0200

    Removed Google provider from OAuth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -50,11 +50,11 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
     })
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET
+    // })
   ],
   theme: {
     colorScheme: 'light'
